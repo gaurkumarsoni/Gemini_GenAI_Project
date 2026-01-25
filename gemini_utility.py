7deefe9be28df1df7af600c9d20b771210abcdd9
Gaurav Kumar <gauravsony67@gmail.com>
2026-01-25 23:48:42 +0530

    ChatBot and Image Captioning added

---
+++ gemini_utility.py
@@ -0,0 +1,45 @@
+import os
+
+import json
+
+from google import genai
+from google.genai import types
+
+
+# Load config
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(working_dir, "config.json"), "r") as f:
+    config_data = json.load(f)
+
+# API key comes automatically from environment OR config
+os.environ["GEMINI_API_KEY"] = config_data["GEMINI_API_KEY"]
+
+# Create client
+client = genai.Client()
+
+
+# function to load gemeni_model for chatbot response
+def get_gemini_response(contents):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=contents
+    )
+    return response.text
+
+# function for image captioning
+def get_image_caption(image):
+    response = client.models.generate_content(
+        model='gemini-3-flash-preview',
+        contents=[
+            types.Part.from_bytes(
+                data=image,
+                mime_type='image/jpeg',
+            ),
+            'Caption this image.'
+        ]
+    )
+
+    return response.text
+
+
---
+++ main.py
@@ -0,0 +1,102 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from gemini_utility import (get_gemini_response , get_image_caption)
+
+# Page configuration
+st.set_page_config(
+    page_title="Gemini-AI",
+    page_icon="🧠",
+    layout="centered"
+)
+
+# Sidebar menu
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Gemini AI",
+        options=[
+            "ChatBot",
+            "Image Captioning",
+            "Embed Text",
+            "Ask me anything"
+        ],
+        icons=[
+            "chat-dots-fill",
+            "image-fill",
+            "textarea-t",
+            "patch-question-fill"
+        ],
+        menu_icon="robot",
+        default_index=0
+    )
+
+# ================= CHATBOT =================
+if selected == "ChatBot":
+
+    st.title("🤖 Gemini ChatBot")
+
+    # Initialize memory
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat history
+    for msg in st.session_state.messages:
+        with st.chat_message(msg["role"]):
+            st.markdown(msg["content"])
+
+    # User input
+    user_prompt = st.chat_input("Ask Gemini...")
+
+    if user_prompt:
+        # Save user message
+        st.session_state.messages.append({
+            "role": "user",
+            "content": user_prompt
+        })
+
+        st.chat_message("user").markdown(user_prompt)
+
+        # Build Gemini contents from history
+        contents = []
+        for msg in st.session_state.messages:
+            contents.append({
+                "role": msg["role"],
+                "parts": [{"text": msg["content"]}]
+            })
+
+            # Get Gemini response
+            response = get_gemini_response(contents)
+
+            # Save assistant reply
+            st.session_state.messages.append({
+                "role": "model",
+                "content": response
+            })
+
+            st.chat_message("model").markdown(response)
+
+
+# ================= IMAGE CAPTIONING =================
+
+if selected == "Image Captioning":
+
+    st.title("🤖 Gemini Image Captioning")
+
+    uploaded_image = st.file_uploader(
+        "Upload an image..",
+        type=["jpg", "jpeg", "png"]
+    )
+
+    if uploaded_image is not None:
+
+        if st.button("Generate Caption"):
+            col1, col2 = st.columns(2)
+            with col1:
+                st.image(uploaded_image, caption="Uploaded Image", width="stretch")
+            with col2:
+                with st.spinner("Gemini is analyzing the image..."):
+                        image_bytes = uploaded_image.getvalue()
+                        caption = get_image_caption(image_bytes)
+                        st.success(caption)
+    else:
+        st.info("👆 Upload an image to get started.")
+
